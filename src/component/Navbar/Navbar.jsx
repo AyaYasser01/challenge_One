01bb0303b439ad296de14bab7a4ceeed4ff21d22
AyaYasser01 <ayayasser920@gmail.com>
2025-05-06 21:28:23 +0300

    Create Navbar component with good style

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 style={{
+      backgroundColor: 'pink',
+      padding: '40px',
+      borderRadius: '20px',
+      textAlign: 'center',
+      color:'white',
+      fontSize: '30px'
+    }}>Tarmeez Academy</h1>
+    </div>
+  )
+}
+
+export default Navbar
